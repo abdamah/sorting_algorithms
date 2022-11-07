db371353f5d0e885859390ea7ef6b0adcee576d6
Abdallah <abdamoh1@gmail.com>
2022-11-07 11:36:23 -0800

    Fix: add description.

--- sort.h
+++ sort.h
@@ -8,11 +8,15 @@
 #define UP 0
 #define DOWN 1
 
-/*enum bool - Enumeration of boolean values*/
+/**
+ * enum bool - Enumeration of boolean values.
+ * @false: falsy with default value 0
+ * @true: truthy value
+ */
 typedef enum bool
 {
-    false = 0,
-    true
+	false = 0,
+	true
 } bool;
 
 
@@ -25,9 +29,9 @@ typedef enum bool
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /*Helper for swapping*/
